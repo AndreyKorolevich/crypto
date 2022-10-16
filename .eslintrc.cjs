4e9793055c00de3e55ce08702685f512e7249752
AndreyKorolevich <korolevich1994@mail.ru>
2022-10-16 13:22:11 -0700

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: ['plugin:vue/essential', 'plugin:prettier/recommended', 'eslint:recommended'],
+  rules: {
+    'vue/padding-line-between-blocks': 'error',
+    'vue/new-line-between-multi-line-property': 'error',
+    'vue/no-dupe-keys': 'off',
+  },
+  env: {
+    'vue/setup-compiler-macros': true,
+  },
+}
